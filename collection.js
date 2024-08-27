b72b10831c3573e5b31ade7d36d0388e4684684a
varun-it21 <varun.it21@bitsathy.ac.in>
2024-08-27 09:42:44 +0530

    first commit

---
+++ collection.js
@@ -0,0 +1,32 @@
+var sidenav = document.querySelector(".side-navbar");
+
+function show() {
+    console.log("called");
+    sidenav.style.left = "0";
+}
+
+function closeWindow() {
+    console.log("close called");
+    sidenav.style.left = "-60%";
+}
+
+var productContainer=document.getElementById("products")
+var search=document.getElementById("search")
+var productlist=productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredvalue=event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count=count+1){
+        var productname = productlist[count].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})
+
+
---
+++ contact.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar");
+
+function show() {
+    console.log("called");
+    sidenav.style.left = "0";
+}
+
+function closeWindow() {
+    console.log("close called");
+    sidenav.style.left = "-60%";
+}
